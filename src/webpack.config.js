9b961db15d2314ea5263404bac3aa9d07ea3999f
Yasir Khalid <yasir_khalid@outlook.com>
2024-07-17 22:49:59 +0000

    restructure project for github pages deployment

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './src/index.js',
   watch: true,
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'js/bundle.js'
   },
   module: {
